65d4bc1e1753c5d3effb7cb50f3e5a93b13cc634
abhishekupadhyay999 <109972672+abhishekupadhyay999@users.noreply.github.com>
2026-07-31 23:45:39 +0530

    Fix DesktopNav JSX closing tags

--- components/home-v2/Navbar/DesktopNav.tsx
+++ components/home-v2/Navbar/DesktopNav.tsx
@@ -29,33 +29,32 @@ export default function DesktopNav({
     >
       <div className="mx-auto flex h-20 w-full max-w-7xl items-center justify-between px-6">
         {/* Logo */}
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo/logo3.png"
+            alt="Carmotive India"
+            width={180}
+            height={60}
+            priority
+            className="h-auto w-40"
+          />
+        </Link>
 
-<Link href="/" className="flex items-center">
+        {/* Navigation */}
+        <div className="flex items-center gap-10">
+          {navLinks.map((item) => (
+            <Link
+              key={item.title}
+              href={item.href}
+              className="group relative text-[15px] font-medium text-white transition"
+            >
+              {item.title}
 
-  <Image
-    src="/logo/logo3.png"
-    alt="Carmotive India"
-    width={180}
-    height={60}
-    priority
-    className="h-auto w-40"
-  />
-
-</Link>
+              <span className="absolute -bottom-2 left-1/2 h-[2px] w-0 -translate-x-1/2 rounded-full bg-[#E50914] transition-all duration-300 group-hover:w-full" />
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className="flex items-center gap-10">
-
-  {navLinks.map((item) => (
-    <Link
-      key={item.title}
-      href={item.href}
-      className="group relative text-[15px] font-medium text-white transition"
-    >
-      {item.title}
-
-      <span className="absolute -bottom-2 left-1/2 h-[2px] w-0 -translate-x-1/2 rounded-full bg-[#E50914] transition-all duration-300 group-hover:w-full" />
-
-    </Link>
-  ))}
-
-</div>
+    </motion.nav>
+  );
+}
