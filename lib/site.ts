a87f905cc00c7fa4c163755823f1e5d95d3de61a
abhishekupadhyay999 <109972672+abhishekupadhyay999@users.noreply.github.com>
2026-08-01 10:07:08 +0530

    Add site configuration

---
+++ lib/site.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "Carmotive",
+  phone: "9876543210",
+  email: "info@carmotive.in",
+  address: "Virar (West), Maharashtra",
+  timings: "Mon - Sun | 10:00 AM - 8:00 PM",
+};
